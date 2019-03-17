a3bbc558a008615c75a1fb26ea16ee3120fdc381
xychenxy <mavischen916@gmail.com>
2019-03-17 21:17:30 +1100

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,44 @@
 module.exports = {
-  apps : [{
-    name: 'API',
-    script: 'app.js',
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }],
+    /**
+     *  Application configuration section
+     *
+     */
+
+  apps : [
+
+
+        // First application
+      {
+        name: 'BootstrapTutorial',
+        script: 'app.js',
+
+        // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+        // args: 'one two',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+          NODE_ENV: 'development'
+        },
+        env_production: {
+          NODE_ENV: 'production'
+        }
+      }
+    ],
 
   deploy : {
     production : {
-      user : 'node',
-      host : '212.83.163.1',
+      user : 'ubuntu',
+      host : '52.23.159.62',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
+      repo : 'https://github.com/xychenxy/BootstrapTutorial.git',
       path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+        env  : {
+            NODE_ENV: 'dev'
+        }
     }
   }
 };
